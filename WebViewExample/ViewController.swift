c1f839ac0b8e999d94730257fcc48498249566b6
shreya1503 <45226306+shreya1503@users.noreply.github.com>
2020-03-24 19:56:08 -0400

    Update ViewController.swift

--- WebViewExample/ViewController.swift
+++ WebViewExample/ViewController.swift
@@ -44,7 +44,12 @@ class ViewController: UIViewController
                 for item in historyList 
                  {
                     print("\(String(describing: item.title)) -  \(item.url.absoluteURL)")
-                }
+                 }
+                 let sb = UIStoryboard(name: "Main", bundle: nil)
+                 let historyTVC = sb.instantiateViewController(identifier: "historyTVC") as! HistoryTableViewController
+                 historyTVC.historyList = self.history
+                 navigationController?.pushViewController(historyTVC, animated: true)
+                 
             }
         }
     }
