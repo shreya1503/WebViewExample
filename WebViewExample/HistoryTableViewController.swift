5eb3a08a60968f611b1ce72911bed9813da2de9f
shreya1503 <45226306+shreya1503@users.noreply.github.com>
2020-03-24 19:53:48 -0400

    Update HistoryTableViewController.swift

--- WebViewExample/HistoryTableViewController.swift
+++ WebViewExample/HistoryTableViewController.swift
@@ -1,12 +1,11 @@
-
 import UIKit
 
 class HistoryTableViewController: UITableViewController
 {
 
-let historyArray : [String]
+let historyArray: [String]
 
-override func numberOfSections(in tableView: UITableView) -> Int
+override func numberOfSections(in tableView: UITableView) -> Int 
 {
     return 1
 }
@@ -14,4 +13,13 @@ override func numberOfSections(in tableView: UITableView) -> Int
 override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
 {
     return historyArray.count
+}
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "BillTableViewCell") as! BillTableViewCell
+         let historyList = historyArray[indexPath.row]
+         cell.textLabel?.text = historyList.title
+         return cell  
+    }
 }
