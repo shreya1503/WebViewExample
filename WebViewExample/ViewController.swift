efcc63892fcddabef03c83e12d8dde6bad66a245
shreya1503 <45226306+shreya1503@users.noreply.github.com>
2020-03-24 18:30:39 -0400

    Update ViewController.swift

--- WebViewExample/ViewController.swift
+++ WebViewExample/ViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import Webkit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadLambtonUrl()
     }
 
+ func loadLambtonUrl()
+    {
+        let url = URL(string: "https://www.lambtoncollege.ca/")
+        let urlReq = URLRequest(url: url!)
+        myWebKitView.load(urlReq)
+    }
+    
+    func loadHtmlString()
+    {
+        let htmlString = "<h1>Hello World</h1>"
+        myWebKitView.loadHTMLString(htmlString, baseURL: nil)
+        
+    }
+    
+    
 
+    @IBAction func btnNavigation(_ sender: UIBarButtonItem)
+    {
+        switch sender.tag 
+        {
+        case 0:
+            myWebKitView.reloadFromOrigin()
+        case 1:
+            if myWebKitView.canGoBack
+            {
+                print("Go back")
+                myWebKitView.goBack()
+            }else
+            {
+                print("Can't Go back")
+            }
+        default:
+            print("No Navigation action found...")
+        }
+    }
+    
 }
 
